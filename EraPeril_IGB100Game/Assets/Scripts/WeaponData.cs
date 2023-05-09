215a73e8e2ba9f90d9eeded25d6e11601126a7f9
Sniffy44 <wcsniffin@gmail.com>
2023-05-10 02:07:35 +1000

    adding axe, sorta

--- EraPeril_IGB100Game/Assets/Scripts/WeaponData.cs
+++ EraPeril_IGB100Game/Assets/Scripts/WeaponData.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 
 [CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon")]
-public class WeaponIndex : ScriptableObject{
+public class WeaponData : ScriptableObject{
     
     public new string name;
     public int damage;
     public int swingTime;
     public bool isThrowable;
+
+    [HideInInspector]
+    public bool isAttacking;
 
 
 
